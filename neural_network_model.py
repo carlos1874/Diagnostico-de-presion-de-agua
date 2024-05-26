9fd1534874a0244448b9422832b5c32d279a96a0
unknown <carlos1874511@gmail.com>
2024-05-26 02:27:11 -0500

    first commit

---
+++ neural_network_model.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Dense, Conv2D, Flatten # type: ignore
+from sklearn.model_selection import train_test_split
+
+class NeuralNetworkModel:
+    def __init__(self):
+        self.model = None
+
+    def build_model(self, input_dim):
+        self.model = Sequential()
+        self.model.add(Dense(64, input_dim=input_dim, activation='relu'))
+        self.model.add(Dense(32, activation='relu'))
+        self.model.add(Dense(1, activation='linear'))
+        self.model.compile(optimizer='adam', loss='mse')
+
+    def train_model(self, X, y, epochs=100, batch_size=10, validation_split=0.2):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
+        loss = self.model.evaluate(X_test, y_test)
+        print(f'Model Loss: {loss}')
+
+    def save_model(self, file_path):
+        self.model.save(file_path)
+
+    def load_model(self, file_path):
+        self.model = tf.keras.models.load_model(file_path)
+
+    def predict(self, input_data):
+        prediction = self.model.predict(input_data)
+        return prediction[0][0]
